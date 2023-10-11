f858e598bacc6518f90e6d1a35419ccaa9750430
maya9906 <mayasato99@gmail.com>
2023-10-10 20:38:30 -0700

    almost done

--- js/weather.js
+++ js/weather.js
@@ -9,21 +9,19 @@ const API_KEY = "7a73f546e23472cce6023cba32b7b261"; // API key for OpenWeatherMa
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
-                    <h4 class="cityName">${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h4>
-                    <div>
+                    <h4 class="cityName">${cityName} </h4>
                     <h5 class="temp">${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h5>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" class="icon" alt="weather-icon">
-                    
-                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
-                    <h6>${weatherItem.weather[0].description}</h6>
+                    <div>
+                        <h6>${weatherItem.weather[0].descreiption}</h6>
+                        <h6>Wind: ${weatherItem.wind.speed} M/S  Humidity: ${weatherItem.main.humidity}%</h6>
                     </div>
                 </div>`;
     } else { // HTML for the other five day forecast card
         return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
@@ -69,13 +67,20 @@ const getCityCoordinates = () => {
     
     // Get entered city coordinates (latitude, longitude, and name) from the API response
     fetch(API_URL).then(response => response.json()).then(data => {
-        if (!data.length) return alert(`No coordinates found for ${cityName}`);
-        const { lat, lon, name } = data[0];
-        getWeatherDetails(name, lat, lon);
-    }).catch(() => {
-        alert("An error occurred while fetching the coordinates!");
-    });
+    if (!data.length) return alert(`No coordinates found for ${cityName}`);
+    const { lat, lon, name } = data[0];
+    getWeatherDetails(name, lat, lon);
+    const fivedays = document.getElementById('fivedays-forecast');
+    fivedays.style.display = 'block'; // 要素を表示
+    
+}).catch(() => {
+    alert("An error occurred while fetching the coordinates!");
+});
 }
+
+
+
+
 
 const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
@@ -105,124 +110,3 @@ cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates
 
 
 
-
-
-
-// let cityInput = document.getElementById("city-input");
-// let searchButton = document.getElementById("getWeatherBtn");
-// let locationButton = document.querySelector(".location-btn");
-// let currentWeatherDiv = document.querySelector(".current-weather");
-// let weatherCardsDiv = document.querySelector(".weather-cards");
-
-// let API_KEY = "7a73f546e23472cce6023cba32b7b261"; // Fixed the missing semicolon here
-
-// let createWeatherCard = (cityName, weatherItem, index) => {
-//     if (index === 0) { //html for the main card
-        
-//         return `<div class="details">
-//                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-//                     <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4> <!-- Fixed the temperature conversion -->
-//                     <h4>Wind: ${weatherItem.wind.speed}M/S </h4>
-//                     <h4>Humidity: ${weatherItem.main.humidity}</h4>
-//                 </div>
-//                 <div class="icon">
-//                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="">
-//                     <h4>${weatherItem.weather[0].description}</h4>
-//                 </div>`;
-        
-//     } else {
-//         return `<li class="card">
-//                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3> <!-- Fixed the closing h3 tag here -->
-//                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png">
-//                     <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4> <!-- Fixed the temperature conversion -->
-//                     <h4>Wind: ${weatherItem.wind.speed}M/S </h4>
-//                     <h4>Humidity: ${weatherItem.main.humidity}</h4>
-//                 </li> `;
-//         // return ""; // Commented out this line to keep the comment.
-//     }
-    
-// }
-// // console.log(createWeatherCard);
-
-// let getWeatherDetails = (cityName, lat, lon) => {
-//     let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-//     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-//         //filter the forecasts to get only one forecast per day
-
-//         let uniqueForecastDays = [];
-//         let fiveDaysForecast =  data.list.filter(forecast => {
-//             let forecastDate = new Date(forecast.dt_txt).getDate(); // Fixed dt_txt typo
-//             if (!uniqueForecastDays.includes(forecastDate)) {
-//                 return uniqueForecastDays.push(forecastDate);
-//             }
-//         });
-
-        
-//         //Clearing previous weather data
-//         cityInput.value = "";
-//         currentWeatherDiv.innerHTML = "";
-//         weatherCardsDiv.innerHTML = "";
-//         //Creating weather cards and adding them to the DOM
-//         fiveDaysForecast.forEach((weatherItem, index) => { // Fixed forEach syntax
-
-//             weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-//             // let cardHtml = createWeatherCard(cityName, weatherItem, index);
-//             // if (cardHtml) {
-//             //     if (index === 0) {
-//             //         currentWeatherDiv.insertAdjacentHTML("beforeend", cardHtml);
-//             //     } else {
-//             //         weatherCardsDiv.insertAdjacentHTML("beforeend", cardHtml);
-//             //     }
-//             // }
-//         });
-//     })
-//     .catch(() => {
-//         alert("An error occurred while fetching the coordinates");
-//     });
-// }
-
-// let getCityCoordinates = () => {
-//     cityName = document.getElementById("city-input").value.trim();
-//     if (!cityName) return; //return if city name is empty
-//     let GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-//     //get entered city coodinates (latitude, longitude, and name) from the API response
-//     fetch(GEOCODING_API_URL)
-//         .then(res => res.json())
-//         .then(data => {
-//             if (!data.length) return alert(`No coordinates found for ${cityName}`);
-//             let { name, lat, lon } = data[0];
-//             getWeatherDetails(name, lat, lon);
-//         })
-//         .catch(() => {
-//             alert("An error occurred while fetching the coordinates");
-//     });
-    
-// }
-
-
-// // let getUserCoordinates = () => {
-// //     navigator.geolocation.getCurrentPosition(
-// //         position => {
-// //             let { latitude, longitude } = position.coords;
-// //             let REVERSE_GEOCORDING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit={limit}&appid=${API_KEY}`
-
-// //             //get city name from coordinates using reverse geocoding API
-// //             fetch(REVERSE_GEOCORDING_URL).then(res => res.json()).then(data => {
-// //                 let { name, lat, lon } = data[0];
-// //                 getWeatherDetails(name, latitude, longitude);
-// //             }).catch(() => {
-// //                 alert("An error occurred while fetching the coordinates");
-// //             });
-// //         },
-// //         error => {
-// //             if(error.code === error.PERMISSION_DENIED) {
-// //                 alert("Geolocation request denied. Please reset location permission to grant access ")
-// //             }
-// //         }
-// //     )
-// // }
-
-// // locationButton.addEventListener("click", getUserCoordinates);
-// searchButton.addEventListener("click", getCityCoordinates);
-// cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-// ローカルストレージ
-
+// ページが読み込まれたときの初期処理
+// Initial actions when the page is loaded
 window.addEventListener('load', () => {
+    // ローカルストレージからTODOリストを取得するか、空の配列を作成
+    // Get the TODO list from local storage or create an empty array
     let todos = JSON.parse(localStorage.getItem('todos')) || [];
     const nameInput = document.querySelector('#name');
     const newTodoForm = document.querySelector('#new-todo-form');
 
+    // ローカルストレージからユーザー名を取得するか、空の文字列を使用
+    // Get the username from local storage or use an empty string
     const username = localStorage.getItem('username') || '';
 
+    // ユーザー名入力フィールドにユーザー名を設定
+    // Set the username in the name input field
     nameInput.value = username;
 
+    // ユーザー名入力フィールドが変更されたときに、ユーザー名をローカルストレージに保存
+    // Save the username to local storage when the name input field is changed
     nameInput.addEventListener('change', e => {
         localStorage.setItem('username', e.target.value);
     });
 
+    // TODO追加フォームが送信されたときの処理
+    // Handling the submission of the TODO form
     newTodoForm.addEventListener('submit', e => {
         e.preventDefault();
 
+        // フォームからTODOの内容とカテゴリを取得
+        // Get the content and category of the TODO from the form
         const todo = {
             content: e.target.elements.content.value,
             category: e.target.elements.category.value,
@@ -23,22 +35,38 @@ window.addEventListener('load', () => {
             createdAt: new Date().getTime()
         };
 
+        // TODOをTODOリストに追加
+        // Add the TODO to the TODO list
         todos.push(todo);
 
+        // TODOリストをローカルストレージに保存
+        // Save the TODO list to local storage
         localStorage.setItem('todos', JSON.stringify(todos));
 
+        // フォームをリセット
+        // Reset the form
         e.target.reset();
 
+        // TODOリストを表示
+        // Display the TODO list
         DisplayTodos();
     });
 
+    // 初回ページ読み込み時にTODOリストを表示
+    // Display the TODO list on the initial page load
     DisplayTodos();
 
+    // TODOリストを表示する関数
+    // Function to display the TODO list
     function DisplayTodos() {
         const todoList = document.querySelector('#todo-list');
 
+        // TODOリストをクリア
+        // Clear the TODO list
         todoList.innerHTML = '';
 
+        // 各TODOアイテムを生成して表示
+        // Generate and display each TODO item
         todos.forEach(todo => {
             const todoItem = document.createElement('div');
             todoItem.classList.add('todo-item');
@@ -66,7 +94,7 @@ window.addEventListener('load', () => {
             edit.classList.add('edit');
             deleteButton.classList.add('delete');
 
-            content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+            content.innerHTML = `<input type='text' value='${todo.content}' readonly>`;
             edit.innerHTML = `Edit`;
             deleteButton.innerHTML = `Delete`;
 
@@ -78,6 +106,8 @@ window.addEventListener('load', () => {
             todoItem.appendChild(content);
             todoItem.appendChild(actions);
 
+            // TODOリストにアイテムを追加
+            // Add items to the TODO list
             todoList.appendChild(todoItem);
 
             if (todo.done) {
@@ -94,32 +124,57 @@ window.addEventListener('load', () => {
                     todoItem.classList.remove('done');
                 }
 
+                // TODOリストを再表示
+                // Re-display the TODO list
                 DisplayTodos();
             });
 
+            // Editボタンがクリックされたときの処理
+            // Handling the click event of the Edit button
             edit.addEventListener('click', e => {
+                // 編集可能な入力フィールドにフォーカスを設定
+                // Set focus on the editable input field
                 const input = content.querySelector('input');
                 input.removeAttribute('readonly');
                 input.focus();
+
+                // 入力フィールドがフォーカスを失ったときの処理
+                // Handling when the input field loses focus
                 input.addEventListener('blur', e => {
+                    // 入力フィールドを読み取り専用に設定
+                    // Set the input field to readonly
                     input.setAttribute('readonly', true);
+
+                    // TODOの内容を更新
+                    // Update the content of the TODO
                     todo.content = e.target.value;
+
+                    // 更新したTODOリストをローカルストレージに保存
+                    // Save the updated TODO list to local storage
                     localStorage.setItem('todos', JSON.stringify(todos));
+
+                    // TODOリストを再表示
+                    // Re-display the TODO list
                     DisplayTodos();
                 });
             });
 
+            // Deleteボタンがクリックされたときの処理
+            // Handling the click event of the Delete button
             deleteButton.addEventListener('click', e => {
+                // TODOリストからこのTODOをフィルタリング
+                // Filter this TODO from the TODO list
                 todos = todos.filter(t => t !== todo);
+
+                // 更新したTODOリストをローカルストレージに保存
+                // Save the updated TODO list to local storage
                 localStorage.setItem('todos', JSON.stringify(todos));
+
+                // TODOリストを再表示
+                // Re-display the TODO list
                 DisplayTodos();
             });
+
         });
     }
 });
-
-function saveButton(){
-    let saveBtn = document.getElementsByClassName("save-btn");
-    
-
-}
